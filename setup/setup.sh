83d7e71aebb213298c3fe465e7b6cd9bcb69dae2
jah377 <jonathan.harris.eit@gmail.com>
2022-05-10 14:30:55 +0200

    finalize setup and venvs

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# # download and install conda
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-# bash Miniconda3-latest-Linux-x86_64.sh # install in /nfs/code/miniconda3
-# rm Miniconda3-latest-Linux-x86_64.sh
-# conda update -n base -c defaults conda -y # update conda
+# download and install conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh # install in /nfs/code/miniconda3
+rm Miniconda3-latest-Linux-x86_64.sh
+conda update -n base -c defaults conda -y # update conda
 
-# export LD_LIBRARY_PATH="/home/jharris/miniconda3/lib:$LD_LIBRARY_PATH"
+# ========================================================================
 
 # create CPU virtual environment
 python3 -m venv venvs/CPU_venv
@@ -19,18 +19,23 @@ venvs/CPU_venv/bin/python3 -m pip install linformer-pytorch einops ogb wandb PyY
 venvs/CPU_venv/bin/python3 -m pip install --upgrade wandb
 wandb login
 
+pip freeze >> setup/requirements_CPU.txt
+
 deactivate 
 
+# ========================================================================
 
 # create GPU virtual environment
 python3 -m venv venvs/GPU_venv
 source venvs/GPU_venv/bin/activate
 
 venvs/GPU_venv/bin/python3 -m pip install --upgrade pip
-venvs/GPU_venv/bin/python3 -m pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+venvs/GPU_venv/bin/python3 -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 venvs/GPU_venv/bin/python3 -m pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.11.0+cu113.html
 venvs/GPU_venv/bin/python3 -m pip install linformer-pytorch einops ogb wandb PyYAML
 venvs/GPU_venv/bin/python3 -m pip install --upgrade wandb
 wandb login
+
+pip freeze >> setup/requirements_GPU.txt
 
 deactivate 
