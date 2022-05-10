fbe0486eefa1c991bdc28aa550144f3a8a8dc0a7
jah377 <jonathan.harris.eit@gmail.com>
2022-05-10 10:35:14 +0200

    fix cpu venv install

---
+++ experiments/sweeps/hps_SIGN.py
@@ -0,0 +1,93 @@
+import wandb
+import torch
+
+from general.models.SIGN import net as SIGN
+from general.utils import set_seeds, build_DataLoader, build_optimizer, build_scheduler
+from general.epoch_steps.steps_SIGN import training_step, testing_step
+
+
+hyperparameter_defaults = dict(
+    dataset='cora',
+    seed=42,
+    optimizer_type='Adam',
+    optimizer_lr=1e-3,
+    optimizer_decay=1e-3,
+    epochs=5,
+    hidden_channel=256,
+    dropout=0.6,
+    K=1,
+    batch_norm=1,
+    batch_size=256,
+)
+# %% 
+import torch
+# %% 
+wandb.init(config=hyperparameter_defaults)
+config = wandb.config
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def main(config):
+    set_seeds(config.seed)
+
+    # data
+    path = f'data/{config.dataset}_sign_k{config.K}.pth'
+    data = torch.load(path)
+    train_dl, val_dl, test_dl = build_DataLoader(data, config.batch_size)
+
+    # model
+    model = SIGN(
+        data.x.shape[1],       # in_channel
+        len(data.y.unique()),  # out_channel
+        config.hidden_channel,
+        config.dropout,
+        config.K,
+        config.batch_norm).to(device)
+
+    # log number of trainable parameters
+    log_dict = {
+        'trainable_params': sum(p.numel() for p in model.parameters() if p.requires_grad)
+    }
+
+    optimizer = build_optimizer(
+        model,
+        config.optimizer_type,
+        config.optimizer_lr,
+        config.optimizer_decay
+    )
+
+    scheduler = build_scheduler(optimizer)
+
+    # train & evaluate
+    previous_loss = 1e10
+    patience = 5
+    trigger_times = 0
+
+    for epoch in range(config.epochs):
+        train_output = training_step(model, data, optimizer, train_dl)
+        val_output = testing_step(model, data, val_dl)
+        test_output = testing_step(model, data, test_dl)
+
+        scheduler.step(val_output['loss'])
+
+        log_dict.update({'epoch': epoch})
+        log_dict.update({'epoch-train_'+k: v for k, v in train_output.items()})
+        log_dict.update({'epoch-val_'+k: v for k, v in val_output.items()})
+        log_dict.update({'epoch-test_'+k: v for k, v in test_output.items()})
+        wandb.log(log_dict)
+
+        # early stopping
+        current_loss = val_output['loss']
+        if current_loss > previous_loss:
+            trigger_times += 1
+            if trigger_times >= patience:
+                print('$$$ EARLY STOPPING TRIGGERED $$$')
+                break
+        else:
+            trigger_times = 0
+        previous_loss = current_loss
+
+
+if __name__ == "__main__":
+    main(config)
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-# download and install conda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh # install in /nfs/code/miniconda3
-rm Miniconda3-latest-Linux-x86_64.sh
-conda update -n base -c defaults conda -y # update conda
+# # download and install conda
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# bash Miniconda3-latest-Linux-x86_64.sh # install in /nfs/code/miniconda3
+# rm Miniconda3-latest-Linux-x86_64.sh
+# conda update -n base -c defaults conda -y # update conda
 
 # export LD_LIBRARY_PATH="/home/jharris/miniconda3/lib:$LD_LIBRARY_PATH"
 
-# create virtual environment -- conda env didn't work due to shell
-python3 -m venv pyg-venv
-source pyg-venv/bin/activate
+# create CPU virtual environment
+python3 -m venv venvs/CPU_venv
+source venvs/CPU_venv/bin/activate
 
-# install dependencies
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
-pip install torch-scatter     -f https://pytorch-geometric.com/whl/torch-1.11.0+cu113.html
-pip install torch-sparse      -f https://pytorch-geometric.com/whl/torch-1.11.0+cu113.html
-pip install torch-cluster     -f https://pytorch-geometric.com/whl/torch-1.11.0+cu113.html
-pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.11.0+cu113.html
-pip install torch-geometric 
-pip install linformer-pytorch 
-pip install einops
-pip install ogb
-pip install wandb
-pip install PyYAML
-pip install --upgrade wandb
-
-# log in to W&B
+venvs/CPU_venv/bin/python3 -m pip install --upgrade pip
+venvs/CPU_venv/bin/python3 -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+venvs/CPU_venv/bin/python3 -m pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.11.0+cpu.html
+venvs/CPU_venv/bin/python3 -m pip install linformer-pytorch einops ogb wandb PyYAML
+venvs/CPU_venv/bin/python3 -m pip install --upgrade wandb
 wandb login
 
+deactivate 
+
+
+# create GPU virtual environment
+python3 -m venv venvs/GPU_venv
+source venvs/GPU_venv/bin/activate
+
+venvs/GPU_venv/bin/python3 -m pip install --upgrade pip
+venvs/GPU_venv/bin/python3 -m pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+venvs/GPU_venv/bin/python3 -m pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.11.0+cu113.html
+venvs/GPU_venv/bin/python3 -m pip install linformer-pytorch einops ogb wandb PyYAML
+venvs/GPU_venv/bin/python3 -m pip install --upgrade wandb
+wandb login
+
+deactivate 
