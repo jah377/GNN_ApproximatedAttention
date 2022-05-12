correct SIGN batch_loss calc

general/epoch_steps/steps_SIGN.py
@@ -25,7 +25,7 @@ def training_step(model, data, optimizer, loader):
     """
     model.train()
 
-    cum_n = cum_loss = cum_correct = 0
+    total_examples = total_loss = total_correct = 0
 
     for idx in loader:
 
@@ -36,23 +36,21 @@ def training_step(model, data, optimizer, loader):
         y = data.y[idx].to(device)              # move target to device
 
         # forward pass
-        batch_logits = model(xs)
-        batch_labels = batch_logits.argmax(dim=-1)
-        batch_loss = F.nll_loss(batch_logits, y)
+        out = model(xs)
+        loss = F.nll_loss(out, y)
 
-        # store
-        cum_n += idx.numel()
-        cum_loss += float(batch_loss) * cum_n
-        cum_correct += sum(batch_labels == y)
+        total_examples += idx.numel()
+        total_loss += float(loss) * idx.numel()
+        total_correct += sum(out.argmax(dim=-1) == y)
 
         # backward pass
         optimizer.zero_grad()
-        batch_loss.backward()
+        loss.backward()
         optimizer.step()
 
     return {
-        'loss': cum_loss/cum_n,
-        'f1': cum_correct/cum_n,
+        'loss': total_loss/total_examples,
+        'f1': total_correct/total_examples,
     }
 
 
@@ -73,27 +71,25 @@ def testing_step(model, data, loader):
     """
     model.eval()
 
-    cum_n = cum_loss = cum_correct = 0
+    total_examples = total_loss = total_correct = 0
 
     for idx in loader:
 
-        # organize
+        # organize data
         xs = [data.x[idx].to(device)]           # add x[idx] to device
         xs += [data[f'x{i}'][idx].to(device)
                for i in range(1, model.K + 1)]  # add each A^K*X[idx] to xs
         y = data.y[idx].to(device)              # move target to device
 
-        # predict
-        batch_logits = model(xs)
-        batch_labels = batch_logits.argmax(dim=-1)
-        batch_loss = F.nll_loss(batch_logits, y)
+        # forward pass
+        out = model(xs)
+        loss = F.nll_loss(out, y)
 
-        # store
-        cum_n += idx.numel()
-        cum_loss += float(batch_loss) * cum_n
-        cum_correct += sum(batch_labels == y)
+        total_examples += idx.numel()
+        total_loss += float(loss) * idx.numel()
+        total_correct += sum(out.argmax(dim=-1) == y)
 
     return {
-        'loss': cum_loss/cum_n,
-        'f1': cum_correct/cum_n,
+        'loss': total_loss/total_examples,
+        'f1': total_correct/total_examples,
     }

general/models/DotProductAttention.py
@@ -10,7 +10,7 @@ from torch_sparse import SparseTensor
 
 class net(nn.Module):
 
-    def __init__(self, d_m: int, num_heads: int = 1, bias: bool = 1):
+    def __init__(self, d_m: int, num_heads: int = 1):
         """
           d_m:          feature embedding dimension
           num_heads:    attn heads (default=1)
@@ -22,16 +22,7 @@ class net(nn.Module):
         self.num_heads = num_heads
         self.d_k = num_heads * d_m      # hidden dim. of proj. subspace
         self.scale = 1.0/np.sqrt(d_m)   # scaling factor per head
-        self.qkv_lin = nn.Linear(d_m, 3*self.d_k, bias=bias) # stacked q,k,v for efficiency
-
-
-    def _reset_parameters(self):
-        # original transformer initialization, see PyTorch documentation
-        nn.init.xavier_uniform_(self.qkv_proj.weight)
-
-        if self.bias == 1:
-            self.qkv_proj.bias.data.fill_(0)
-
+        self.qkv_lin = nn.Linear(d_m, 3*self.d_k) # stacked q,k,v for efficiency
 
     def forward(self, x, edge_index):
         """
@@ -60,7 +51,9 @@ class net(nn.Module):
               torch.ones_like(edge_index[1]),
               size=attn.shape[1:], # L x L
               ).coalesce() # sparse mask for single head 
+
         S = torch.stack([S for _ in range(self.num_heads)]).coalesce() # sparse mask for all heads
+
         attn = attn.sparse_mask(S).to_dense() # mask non-edge attn values per head
 
         attn = attn.div( reduce(attn, 'h Li Lj -> h Li 1', 'sum') ) # normalize by row

general/models/SamplerGAT.py
@@ -96,7 +96,6 @@ class net(torch.nn.Module):
 
         return F.log_softmax(x_all, dim=-1)
 
-    @resources
     @torch.no_grad()
     def extract_features(self, x_all, subgraph_loader):
         """
@@ -107,8 +106,8 @@ class net(torch.nn.Module):
 
         # to store edge_index attention weights and occurances
         dim = subgraph_loader.data.num_nodes
-        attn_sum = torch.sparse_coo_tensor(size=(dim, dim)).to(cpu)
-        count_sum = torch.sparse_coo_tensor(size=(dim, dim)).to(cpu)
+        attn_coo = torch.sparse_coo_tensor(size=(dim, dim)).to(cpu)
+        count_total = torch.sparse_coo_tensor(size=(dim, dim)).to(cpu)
 
         for i, conv in enumerate(self.convs):
             xs = []
@@ -132,7 +131,7 @@ class net(torch.nn.Module):
                     indices = batch.n_id[attn_i].to(cpu)
                     values = attn_w.mean(dim=1).detach().to(cpu)
 
-                    attn_total += torch.sparse_coo_tensor(
+                    attn_coo += torch.sparse_coo_tensor(
                         indices,
                         values,
                         size=(dim, dim)
@@ -148,14 +147,13 @@ class net(torch.nn.Module):
             x_all = torch.cat(xs, dim=0)
 
         # average attention = attn_total / count_total
-        attn_sum = attn_sum.multiply(count_sum.float_power(-1)).coalesce()
+        attn_coo = attn_coo.multiply(count_total.float_power(-1)).coalesce()
 
-        # convert to SparseTensor to replace adj_t
-        attn_sparse = SparseTensor(
-            row=attn_sum.indices()[0],
-            col=attn_sum.indices()[1],
-            values=attn_sum.values(),
+        r, c = attn_coo.indices()
+
+        return SparseTensor(
+            row=r,
+            col=c,
+            value=attn_coo.values().detach(),
             sparse_sizes=(dim, dim)
-        )
-
-        return attn_sparse
+        )  # to replace adj_t

general/transforms/transforms_SamplerGAT.py
@@ -176,7 +176,7 @@ def extract_attention(data, GATtransform_params):
             trigger_times = 0
         previous_loss = current_loss
 
-    # get attention torch.sparse_coo_matrix
+    # get attention SparseTensor
     model.eval()
     attn_sparse = model.extract_features(data.x, subgraph_loader)
 

general/utils.py
@@ -72,25 +72,6 @@ def set_seeds(seed_value: int):
         torch.cuda.manual_seed_all(seed_value)
 
 
-# def get_data(dataset_name, transform=None):
-#     dataset_name = dataset_name.lower()
-#     assert dataset_name in ['cora', 'pubmed']
-
-#     path = os.path.join(os. getcwd(), 'data', 'test')
-
-#     if dataset_name == 'pubmed':
-#         datamodule = Planetoid(root=path,
-#                                name='PubMed',
-#                                split='full',
-#                                transform=transform)[0]
-#     elif dataset_name == 'cora':
-#         datamodule = Planetoid(root=path,
-#                                name='Cora',
-#                                split='full',
-#                                transform=transform)[0]
-#     return datamodule
-
-
 def build_optimizer(model, name: str, learning_rate: float, weight_decay: float):
     """ build optimizer
     Args:
@@ -132,11 +113,11 @@ def build_scheduler(optimizer):
     )
 
 
-def build_DataLoader(data, batch_size: int):
+def build_DataLoader(data, batch_size: int, dataset_name: str = None):
     """ Create train/val/test DataLoader for SIGN
 
     Args:
-        data:           data object
+        idx:            all idx values
         batch_size:     int, batch_size
 
     Returns:
@@ -145,18 +126,23 @@ def build_DataLoader(data, batch_size: int):
         test_loader:
     """
 
-    def get_idx(data, split):
-        return eval(
-            f"data.{split}_mask.nonzero(as_tuple=False).view(-1)"
-        )
+    if dataset_name == 'products':
+        split_idx = data.get_idx_split()
+        def get_idx(_, split): return split_idx[split]
+    else:
+        def get_idx(data, split): return eval(
+            f"data.{split}_mask.nonzero(as_tuple=False).view(-1)")
 
     def loader(data, split):
+        if dataset_name == 'product':
+            if split == 'val':
+                split = 'valid'
 
-        return DataLoader(
-            get_idx(data, split),
-            batch_size=batch_size,
-            shuffle=(split == 'train'),     # shuffle if training loader
-            drop_last=(split == 'train'),   # remove the final incomplete batch
-        )
+            return DataLoader(
+                get_idx(data, split),
+                batch_size=batch_size,
+                shuffle=(split == 'train'),   # shuffle if training loader
+                drop_last=(split == 'train'), # remove final incomplete
+            )
 
     return [loader(data, split) for split in ['train', 'val', 'test']]

setup/generate_data.py
@@ -60,10 +60,9 @@ def make_products(folder_path):
             'ogbn-products', 
             root='/tmp/Products', 
             transform=transform)
-        data = dataset[0]
-
+        
         filename = osp.join(folder_path, f'products_sign_k{K}.pth')
-        torch.save(data, filename)
+        torch.save(dataset, filename)
 
 
 if __name__ == '__main__':